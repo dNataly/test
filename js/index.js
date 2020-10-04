5c5c20a1d8bcb9f8b93f33745edff7b3d0378178
Natasha <nnmtshh.gmail.com>
2020-10-05 00:20:31 +0300

    done with dropdown

--- js/index.js
+++ js/index.js
@@ -1,1 +1,26 @@
-onscroll="document.getElementById('subtask-li').scrollLeft = this.scrollLeft" 
+const allDropdowns = document.querySelectorAll(".dropdown");
+
+if (allDropdowns != null) {
+  allDropdowns.forEach(dd => {
+    const button = dd.querySelector(".dropdown-button");
+    const menu = dd.querySelector(".dropdown-menu");
+
+    button.onclick = function() {
+      if (menu.style.display === "none" || menu.style.display === "") {
+        menu.style.display = "inline-block";
+      } else {
+        menu.style.display = "none";
+      }
+    }
+    const options = menu.querySelectorAll(".dropdown-option");
+
+    if (options != null) {
+      options.forEach(o => {
+				o.onclick = function () {
+        	button.innerHTML = o.innerHTML;
+          menu.style.display = "none";
+        }
+      })
+    }
+  })
+}
