58b95ef4079d88de69a75f64a8af9df91ca2dd0e
Natasha <nnmtshh.gmail.com>
2020-10-12 20:16:31 +0300

    check

--- js/swipe.js
+++ js/swipe.js
@@ -1,40 +1,45 @@
-item.addEventListener('touchstart', handleTouchStart, false);        
-		item.addEventListener('touchmove', handleTouchMove, false);
-		var xDown = null;
-		var yDown = null;
-		function handleTouchStart(evt) {
-			xDown = evt.touches[0].clientX;
-			yDown = evt.touches[0].clientY;
-		};
-		function handleTouchMove(evt) {
-			if ( ! xDown || ! yDown ) {
-				return;
-			}
+document.addEventListener('touchstart', handleTouchStart, false);        
+document.addEventListener('touchmove', handleTouchMove, false);
 
-			var xUp = evt.touches[0].clientX;
-			var yUp = evt.touches[0].clientY;
+var xDown = null;                                                        
+var yDown = null;
 
-			var xDiff = xDown - xUp;
-			var yDiff = yDown - yUp;
+function getTouches(evt) {
+  return evt.touches ||             // browser API
+         evt.originalEvent.touches; // jQuery
+}                                                     
 
-			if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
-				if ( xDiff > 0 ) {
-					/* left swipe */
-					navigate('right');
-				} else {
-					navigate('left');
-				}
-			} 
-			/* reset values */
-			xDown = null;
-			yDown = null;
-		};
+function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0];                                      
+    xDown = firstTouch.clientX;                                      
+    yDown = firstTouch.clientY;                                      
+};                                                
 
+function handleTouchMove(evt) {
+    if ( ! xDown || ! yDown ) {
+        return;
+    }
 
-	}
+    var xUp = evt.touches[0].clientX;                                    
+    var yUp = evt.touches[0].clientY;
 
-	[].slice.call(document.querySelectorAll('.slideshow-container')).forEach( function(item) {
-		init(item);
-	});
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
 
-})();
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+        if ( xDiff > 0 ) {
+            /* left swipe */ 
+        } else {
+            /* right swipe */
+        }                       
+    } else {
+        if ( yDiff > 0 ) {
+            /* up swipe */ 
+        } else { 
+            /* down swipe */
+        }                                                                 
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;                                             
+};
