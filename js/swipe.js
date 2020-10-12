c24e9991821d42370e1a4aa9182e33549e290701
Natasha <nnmtshh.gmail.com>
2020-10-12 20:05:56 +0300

    check swipe

---
+++ js/swipe.js
@@ -0,0 +1,40 @@
+item.addEventListener('touchstart', handleTouchStart, false);        
+		item.addEventListener('touchmove', handleTouchMove, false);
+		var xDown = null;
+		var yDown = null;
+		function handleTouchStart(evt) {
+			xDown = evt.touches[0].clientX;
+			yDown = evt.touches[0].clientY;
+		};
+		function handleTouchMove(evt) {
+			if ( ! xDown || ! yDown ) {
+				return;
+			}
+
+			var xUp = evt.touches[0].clientX;
+			var yUp = evt.touches[0].clientY;
+
+			var xDiff = xDown - xUp;
+			var yDiff = yDown - yUp;
+
+			if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+				if ( xDiff > 0 ) {
+					/* left swipe */
+					navigate('right');
+				} else {
+					navigate('left');
+				}
+			} 
+			/* reset values */
+			xDown = null;
+			yDown = null;
+		};
+
+
+	}
+
+	[].slice.call(document.querySelectorAll('.slideshow-container')).forEach( function(item) {
+		init(item);
+	});
+
+})();
